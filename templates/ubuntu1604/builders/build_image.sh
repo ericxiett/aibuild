255bc379c1bfd3bca442b320f84d24d64b9edafd
ericxiett <eric_xiett@163.com>
2018-11-09 15:54:10 +0800

    Add installation of acpid

--- templates/ubuntu1604/builders/build_image.sh
+++ templates/ubuntu1604/builders/build_image.sh
@@ -7,7 +7,7 @@ if [[ ! -x $PACKER_EXEC ]]; then
 fi
 echo $PACKER_EXEC
 
-ISOURL=http://releases.ubuntu.com/16.04.5/ubuntu-16.04.5-server-amd64.iso
+ISOURL=http://10.2.32.9/isos/ubuntu-16.04.5-server-amd64.iso
 OUTDIR=/tmp/ubuntu1604-$BUILD_TAG
 IMGNAME=ubuntu1604x86_64-$BUILD_TAG.qcow2
 CHANGE=$CHANGE_TITLE
