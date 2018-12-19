97eee85db0c50701df450f49bd2631abfcaf32f1
ericxiett <eric_xiett@163.com>
2018-12-19 22:42:35 +0800

    Add build

---
+++ image_templates/opensuse422/builders/build_image.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+PACKER_EXEC=/opt/packer
+if [[ ! -x $PACKER_EXEC ]]; then
+    echo "Packer not found!"
+    exit 1
+fi
+echo $PACKER_EXEC
+
+OSNAME="opensuse422"
+ISOURL=$ISOS_URL
+WEBSERVER=$WEB_SERVER
+OUTDIR=/tmp/$OSNAME"_"$BUILD_NUMBER
+IMGNAME=$OSNAME"x86_64_"`date +%Y%m%d`"_"$BUILD_NUMBER.qcow2
+
+echo "Build: "$BUILD_NUMBER
+echo "GIT_COMMIT: "$GIT_COMMIT
+
+echo "Workspace: "$WORKSPACE
+cd $WORKSPACE
+$PACKER_EXEC build -var "outdir=$OUTDIR" -var "vmname=$IMGNAME" -var "isourl=$ISOURL" opensuse422x86_64.json
+
+generate_post_data()
+{
+    cat <<EOF
+{
+    "image_name":"$IMGNAME",
+    "os_name":"$OSNAME",
+    "update_contents":"$GIT_COMMIT",
+    "get_url": "http://$WEB_SERVER/images/$OSNAME/$IMGNAME"
+}
+EOF
+}
+
+if [[ -e "$OUTDIR/$IMGNAME" ]]; then
+    # Need add dns mapping for aibuild-server.com and hostip
+    curl --header "Content-Type: application/json" \
+      --request POST \
+      --data "$(generate_post_data)" \
+      http://aibuild-server.com:9753/v1/build
+
+    virt-sysprep -a $OUTDIR/$IMGNAME
+    # Move image to build dir
+    WEBPATH="/var/www/html/images/$OSNAME"
+    mkdir -p $WEBPATH
+    mv $OUTDIR/$IMGNAME $WEBPATH
+else
+    echo "Image not generated successfully"
+    exit 1
+fi
+
+# Clean
+rm -rf $OUTDIR
