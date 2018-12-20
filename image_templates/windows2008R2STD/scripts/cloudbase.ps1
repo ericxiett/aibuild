81a04984bbadce928df7e038bbea587ad69bd5ac
ericxiett <eric_xiett@163.com>
2018-12-20 19:26:01 +0800

    Add templates of windows 2008R2 standard

---
+++ image_templates/windows2008R2STD/scripts/cloudbase.ps1
@@ -0,0 +1,25 @@
+$Host.UI.RawUI.WindowTitle = "Downloading Cloudbase-Init..."
+
+$url = "http://10.2.32.9/soft/CloudbaseInitSetup_Stable_x64.msi"
+(new-object System.Net.WebClient).DownloadFile($url, "C:\Windows\Temp\cloudbase-init.msi")
+
+$Host.UI.RawUI.WindowTitle = "Installing Cloudbase-Init..."
+
+$serialPortName = @(Get-WmiObject Win32_SerialPort)[0].DeviceId
+
+$p = Start-Process -Wait -PassThru -FilePath msiexec -ArgumentList "/i C:\Windows\Temp\cloudbase-init.msi /qn /l*v C:\Windows\Temp\cloudbase-init.log LOGGINGSERIALPORTNAME=$serialPortName USERNAME=Administrator"
+if ($p.ExitCode -ne 0) {
+    throw "Installing Cloudbase-Init failed. Log: C:\Windows\Temp\cloudbase-init.log"
+}
+
+Copy-Item "A:\cloudbase-init.conf" -Destination "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf"
+# Copy-Item "A:\ICPG2Run.ps1" -Destination "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\LocalScripts\"
+
+$Host.UI.RawUI.WindowTitle = "Running Cloudbase-Init SetSetupComplete..."
+& "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\bin\SetSetupComplete.cmd"
+
+(Get-WmiObject -class Win32_TSGeneralSetting -Namespace root\cimv2\terminalservices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0)
+
+$Host.UI.RawUI.WindowTitle = "Running Sysprep..."
+$unattendedXmlPath = "${env:ProgramFiles}\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml"
+& "${env:SystemRoot}\System32\Sysprep\Sysprep.exe" `/generalize `/oobe `/shutdown `/unattend:"$unattendedXmlPath"
---
+++ image_templates/windows2008R2STD/scripts/installdriver.ps1
@@ -0,0 +1,44 @@
+function UnzipFile([string]$souceFile, [string]$targetFolder)
+{
+    if(!(Test-Path $targetFolder))
+    {
+        mkdir $targetFolder
+    }
+    $shellApp = New-Object -ComObject Shell.Application
+    $files = $shellApp.NameSpace($souceFile).Items()
+    #下面这句会删除已解压的，但不会影响目录内其它不相干的文件      
+    $files|%{if (Test-Path ("$targetFolder/{0}" -f  $_.name )){Remove-Item ("$targetFolder/{0}" -f  $_.name) -Force -Recurse}}
+    $shellApp.NameSpace($targetFolder).CopyHere($files)
+}
+
+# 针对windows 2008 Disable Network Level Authentication 否则会提示安全漏洞
+(Get-WmiObject -class Win32_TSGeneralSetting -Namespace root\cimv2\terminalservices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0)
+
+
+Import-Certificate -file "a:\redhat.cer" -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
+$url = "http://10.2.32.9/soft/cloud_init/2018/windows/2k8R2/Balloon.zip"
+(new-object System.Net.WebClient).DownloadFile($url, "C:\Windows\Temp\Balloon.zip")
+
+UnzipFile "C:\Windows\Temp\Balloon.zip" "C:\Windows\Temp\"
+PnPutil.exe -i -a "C:\Windows\Temp\balloon.inf"
+
+
+# 下载串口驱动
+$url = "http://10.2.32.9/soft/cloud_init/2018/windows/2k8R2/vioserial.zip"
+(new-object System.Net.WebClient).DownloadFile($url, "C:\Windows\Temp\vioserial.zip")
+
+## 下载串口ddl
+$url = "http://10.2.32.9/soft/cloud_init/2018/windows/dlls.zip"
+(new-object System.Net.WebClient).DownloadFile($url, "C:\Windows\Temp\dlls.zip")
+UnzipFile "C:\Windows\Temp\dlls.zip" "C:\Windows\Temp\"
+
+Move-Item -Path "C:\Windows\Temp\dlls\*" -Destination "C:\Windows\System32\"
+
+# 安装驱动
+UnzipFile "C:\Windows\Temp\vioserial.zip" "C:\Windows\Temp\"
+PnPutil.exe -i -a "C:\Windows\Temp\vioserial\2k8R2\amd64\vioser.inf"
+
+# 安装Qga
+$url = "http://10.2.32.9/soft/qga/windows/qemu-ga-x64.msi"
+(new-object System.Net.WebClient).DownloadFile($url, "C:\Windows\Temp\qemu-ga-x64.msi")
+Start-Process C:\Windows\Temp\qemu-ga-x64.msi /qn -Wait
